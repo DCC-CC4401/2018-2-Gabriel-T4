228c5c59bb7976eec7ba004907d43e9a8d8536c8
jorgeToloza <jorge.toloza.v@ug.uchile.cl>
2018-11-26 19:25:59 -0300

    Modelo Relacional Consistente

--- proyecto/coevaluaciones/models.py
+++ proyecto/coevaluaciones/models.py
@@ -4,28 +4,18 @@ from django.db import models
 
 
 class Usuario(models.Model):
-    estudiante = 1
-    profesor = 2
-    auxiliar = 3
-    ayudante = 4
-    tipo = ((estudiante, 1),
-            (profesor, 2),
-            (auxiliar, 3),
-            (ayudante, 4),)
-    rut = models.IntegerField()
-    # TODO cambiar almacenamiento contraseña VERSION MUY PRELIMINAR, ESTO HAY QUE CAMBIARLO
-    password = models.CharField(max_length=30)
+    rut = models.CharField(primary_key=True, max_length=10)
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
-    tipo_usuario = models.IntegerField(choices=tipo)
+    password = models.CharField(max_length=30)
 
     def __str__(self):
         return '{}, {}'.format(self.apellido, self.nombre)
 
 
 class Ramo(models.Model):
-    codigo = models.CharField(max_length=10)
+    codigo = models.CharField(primary_key=True, max_length=10)
     nombre = models.CharField(max_length=30)
 
     def __str__(self):
@@ -33,111 +23,136 @@ class Ramo(models.Model):
 
 
 class Curso(models.Model):
-    # TODO revisar on_delete
-    codigo_ramo = models.ForeignKey(Ramo, on_delete=models.DO_NOTHING)
+    ramo = models.ForeignKey(Ramo, on_delete=models.SET_NULL, null=True)
     anno = models.IntegerField()
     semestre = models.IntegerField()
     seccion = models.IntegerField()
-    alumnos = models.ManyToManyField(Usuario, related_name='lista_alumnos')
-    profesores = models.ManyToManyField(Usuario, related_name='lista_profesores')
-    auxiliares = models.ManyToManyField(Usuario, related_name='lista_auxiliares')
-    ayudantes = models.ManyToManyField(Usuario, related_name='lista_ayudantes')
+    listaDeAlumnos = models.OneToOneField('listaAlumnos', related_name='lista_alumnos', blank=True, null=True,
+                                          on_delete=models.DO_NOTHING)
+    listaDeProfesores= models.OneToOneField('listaProfesores', related_name='lista_profesores', blank=True,
+                                            null=True, on_delete=models.DO_NOTHING)
+    listaDeAuxiliares = models.OneToOneField('listaAuxiliares', related_name='lista_auxiliares', blank=True,
+                                             null=True, on_delete=models.DO_NOTHING)
+    listaDeAyudantes = models.OneToOneField('listaAyudantes', related_name='lista_ayudantes', blank=True,
+                                            null=True, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        unique_together = ('ramo', 'anno', 'semestre', 'seccion')
 
     def __str__(self):
-        return '{}-{} {} {}, {}'.format(self.codigo_ramo,
-                                        self.seccion,
-                                        Ramo.objects.get(codigo=self.codigo_ramo),
-                                        self.anno,
-                                        'Otoño' if self.semestre == 0 else 'Primavera')
+        return '{}-{} {} {}, {}'.format(self.ramo.codigo,
+                                     self.seccion,
+                                     self.ramo.nombre,
+                                     self.anno,
+                                     'Otoño' if self.semestre == 0 else 'Primavera')
 
 
-class Grupo(models.Model):
-    # identificacion del curso al que perteneces
-    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
-
-    # identificacion del grupo
+class Grupo (models.Model):
+    integrantes = models.ForeignKey(Usuario, null=True, on_delete=models.DO_NOTHING)
     nombre = models.CharField(max_length=30)
-    integrantes = models.ManyToManyField(Usuario)
-
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
     activo = models.BooleanField()
 
     def __str__(self):
-        return self.nombre
+        return str(self.nombre)
 
 
-class HistorialRoles(models.Model):
-    estudiante = 1
-    profesor = 2
-    auxiliar = 3
-    ayudante = 4
-    tipo = ((estudiante, 1),
-            (profesor, 2),
-            (auxiliar, 3),
-            (ayudante, 4),)
-    borrar = 0
-    agregar = 1
-    acciones = ((borrar, 0),
-                (agregar, 1),)
-
-    # llaves que identifican el usuario y el curso en el que se realiza la modificacion
-    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
-    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
-    # TODO usar choices en los models
-    # accion que se realiza, deberiamos usar EnumTypes pa identificar entre
-    # estudiante, prof, ... y para accion agregado, borrado
-
-    accion = models.IntegerField(choices=acciones)
-    rol = models.IntegerField(choices=tipo)
-
-    # tiempo en el que ocurrio la modificacion
-    fecha = models.DateTimeField()
+class ListaAlumnos(models.Model):
+    curso = models.OneToOneField(Curso, on_delete=models.DO_NOTHING)
+    alumnos = models.ManyToManyField(Usuario)
 
     def __str__(self):
-        return self.fecha
-
-    class Meta:
-        verbose_name_plural = "HistorialRoles"
+        return 'Lista de Alumnos de: ' + str(self.curso.ramo.codigo)
 
 
-class HistorialGrupos(models.Model):
-    borrar = 0
-    agregar = 1
-    crear = 2
-    acciones = ((borrar, 0),
-                (agregar, 1),
-                (crear, 2),)
-
-    # identificacion del curso al que perteneces el grupo donde se hizo la modificacion
-    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
-
-    # identificacion del grupo en donde ocurrio el cambio
-    grupo = models.ForeignKey(Grupo, on_delete=models.DO_NOTHING)
-
-    # nuevo estado del grupo
-    nombre_grupo = models.CharField(max_length=30)
-    integrantes = models.ManyToManyField(Usuario)
-    # TODO usar choices
-    accion = models.IntegerField(choices=acciones)
+class ListaProfesores(models.Model):
+    curso = models.OneToOneField(Curso, on_delete=models.DO_NOTHING)
+    profesores = models.ManyToManyField(Usuario)
 
     def __str__(self):
-        return self.fecha
-
-    class Meta:
-        verbose_name_plural = "HistorialGrupos"
+        return 'Lista de Profesores de: ' + str(self.curso.ramo.codigo)
 
 
-# aca empieza lo relacionado con las coevaluaciones, todavia no se hace el link entre coevaluacion y ramos
+class ListaAuxiliares(models.Model):
+    curso = models.OneToOneField(Curso, on_delete=models.DO_NOTHING)
+    auxiliares = models.ManyToManyField(Usuario)
+
+    def __str__(self):
+        return 'Lista de Axuliares de: ' + str(self.curso.ramo.codigo)
+
+
+class ListaAyudantes(models.Model):
+    curso = models.OneToOneField(Curso, on_delete=models.DO_NOTHING)
+    ayudantes = models.ManyToManyField(Usuario)
+
+    def __str__(self):
+        return 'Lista de Ayudantes de: ' + str(self.curso.ramo.codigo)
+
+
 class Pregunta(models.Model):
-    tipo = models.CharField(max_length=10)
-    texto = models.TextField()
+    interrogante = models.TextField(default='Escriba la interrogante a preguntar')
+    nota = 'nota'
+    texto = 'texto'
+    tipos = ((nota, 'nota'),
+             (texto, 'texto'),)
+    tipo = models.CharField(max_length=10, choices=tipos)
 
     def __str__(self):
-        return 'Pregunta ' + self.id
+        return 'Pregunta ' + str(self.id)
 
 
 class Cuestionario(models.Model):
     nombre = models.CharField(max_length=30)
-    preguntas = models.ManyToManyField(Pregunta)
+    listaDePreguntas= models.ManyToManyField(Pregunta, blank=True)
 
     def __str__(self):
         return self.nombre
+
+
+class Coevaluaciones(models.Model):
+    id_Coeavaluacion = models.IntegerField(primary_key=True)
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateTimeField()
+    estado = models.BooleanField()
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
+    cuestionario = models.ForeignKey(Cuestionario, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return 'Coevaluación ' + self.curso.ramo.nombre + ': ' + self.cuestionario.nombre
+
+
+class HistorialAlumnos(models.Model):
+    usuario = models.OneToOneField(Usuario, null=True, on_delete=models.DO_NOTHING)
+    cursos = models.ForeignKey(Curso, null=True, on_delete=models.DO_NOTHING)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Ultima modificación: ' + str(self.fecha)
+
+
+class HistorialCursos(models.Model):
+    rut = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
+    grupo = models.ForeignKey(Grupo, on_delete=models.DO_NOTHING)
+    borrado = 0
+    agregado = 1
+    creado = 2
+    acciones = ((borrado, 0),
+                (agregado, 1),
+                (creado, 2),)
+    accion = models.IntegerField(choices=acciones)
+    fecha = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Última modificación: ' + str(self.fecha)
+
+
+class Resultados(models.Model):
+    idCoev = models.ForeignKey(Coevaluaciones, on_delete=models.DO_NOTHING)
+    rut_evaluador = models.ForeignKey(Usuario, related_name='evaluador', on_delete=models.DO_NOTHING)
+    rut_evaluado = models.ForeignKey(Usuario, related_name='evaluado', on_delete=models.DO_NOTHING)
+    preguntas = models.ForeignKey(Cuestionario, on_delete=models.DO_NOTHING)
+    respuesta = models.TextField()
+
+    def __str__(self):
+        return 'Resultados de ' + str(self.rut_evaluado)
--- proyecto/coevaluaciones/views.py
+++ proyecto/coevaluaciones/views.py
@@ -1,9 +1,33 @@
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
 from django.shortcuts import render
+
+from coevaluaciones.models import Usuario
 
 
 # Create your views here.
 def index(request):
-    return render(request, '../Templates/home-vista-alumno.html', {})
+    return render(request, '../Templates/login.html', {})
 
+
+def inicio_sesion(request):
+    if request.POST:
+        username = request.POST.get('inputUser')
+        password = request.POST.get('inputPassword')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+        usuario = Usuario.objects.get(User=user)
+
+        return HttpResponseRedirect(reverse('usuarios:perfil', kwargs={'usuario_id': usuario.id}))
+
+    logout(request)
+
+    return render(request, '/Templates/home-vista-alumno.html', {})
 
